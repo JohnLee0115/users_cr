eb511f379d656f5bc64d6ccc30a098f989e0c449
johnlee0115 <johnlee011500@gmail.com>
2023-06-15 03:17:30 -0700

    crud

--- server.py
+++ server.py
@@ -4,6 +4,10 @@ app.secret_key = "boonk gang"
 
 from user_model import User
 
+@app.route('/')
+def sendtousers():
+    return redirect('/users')
+
 @app.route('/users/new')
 def show_form():
 
@@ -23,7 +27,33 @@ def index():
 
     return render_template("index.html", all_users = all_users)
 
+@app.route('/users/<int:user_id>/')
+def show_user(user_id):
+    
+    one_user = User.get_one({'user_id' : user_id})
 
+    return render_template('show.html', one_user = one_user)
+
+@app.route('/users/<int:user_id>/edit/')
+def show_edit_form(user_id):
+
+    one_user = User.get_one({'user_id' : user_id})
+
+    return render_template('edit_form.html', one_user=one_user)
+
+@app.route('/edit_users', methods = ['POST'])
+def edit_user():
+    
+    User.update_user(request.form)
+
+    return redirect('/users/')
+
+@app.route('/delete/<int:user_id>/')
+def deletus_user(user_id):
+
+    User.delete_user({'user_id' : user_id})
+
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
